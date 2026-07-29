c689aea3925d2382511a979da25126018159cb12
Nadia Parsa <nadiaparsa77@gmail.com>
2026-07-29 13:24:51 +0200

    Update main.py

--- project-02-Second_Largest_Number/main.py
+++ project-02-Second_Largest_Number/main.py
@@ -1,1 +1,17 @@
+def second_largest(lst):
+  if not lst or len(lst) < 2:
+    return None
 
+  largest = None
+  second = None
+
+  for num in lst:
+    if largest is None or num > largest:
+      largest = num
+
+  for num in lst:
+    if num != largest:
+      if second is None or num > second:
+        second = num 
+  
+  return second
